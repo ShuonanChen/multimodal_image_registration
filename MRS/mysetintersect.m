66f18c7782da7dd5d71d655d9c33458f7b845910
ShuonanChen <22083756+ShuonanChen@users.noreply.github.com>
2023-10-03 13:18:37 -0400

    Delete MRS directory

--- MRS/mysetintersect.m
+++
@@ -1,5 +0,0 @@
-function [out1,out2]=mykeepintersect(x,y)
-a=ismember(x,y,'rows');
-out1=x(a==1,1);
-out2=x(a==1,2);
-end
--- MRS/norms.m
+++
@@ -1,4 +0,0 @@
-function n=norms(x,dim)
-% row-wise norms
-n=sqrt(sum(x.^2,dim));
-end
